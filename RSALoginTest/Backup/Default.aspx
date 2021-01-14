be555d03d76b2e0cf1e0ede05682e36112b89696
LittleNewGod <liuqingxin810@163.com>
2021-01-14 16:54:05 +0800

    RSA前后端加密解密
    C#,Webform,JS，加密解密

---
+++ RSALoginTest/Backup/Default.aspx
@@ -0,0 +1,53 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="RSALoginTest._Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>RSA Login Test</title>
+    <script src="Scripts/jquery-1.4.1.js" type="text/javascript"></script>
+    <script src="Scripts/jQuery.md5.js" type="text/javascript" ></script>
+    <script src="Scripts/BigInt.js" type="text/javascript"></script>
+    <script src="Scripts/RSA.js" type="text/javascript"></script>
+    <script src="Scripts/Barrett.js" type="text/javascript"></script>
+    <script type="text/javascript">
+        function cmdEncrypt() {
+            setMaxDigits(129);
+            var key = new RSAKeyPair("<%=strPublicKeyExponent%>", "", "<%=strPublicKeyModulus%>");
+            var pwdMD5Twice = $.md5($.md5($("#txtPassword").attr("value")));
+            var pwdRtn = encryptedString(key, pwdMD5Twice);
+            $("#encrypted_pwd").attr("value", pwdRtn);
+            $("#formLogin").submit();
+            return;
+        }
+    </script>
+
+</head>
+<body>
+    <form action="Default.aspx" id="formLogin" method="post">
+    <div>
+        <div>
+            User Name:
+        </div>
+        <div>
+            <input id="txtUserName" name="txtUserName" value="<%=postbackUserName%>" type="text" maxlength="16" />
+        </div>
+        <div>
+            Password:
+        </div>
+        <div>
+            <input id="txtPassword" type="password" maxlength="16" />
+        </div>
+        <div>
+            <input id="btnLogin" type="button" value="Login" onclick="return cmdEncrypt()" />
+        </div>
+    </div>
+    <div>
+        <input type="hidden" name="encrypted_pwd" id="encrypted_pwd" />
+    </div>
+    </form>
+    <div>
+        <%=LoginResult%>
+    </div>
+</body>
+</html>
---
+++ RSALoginTest/Backup/Default.aspx.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Security.Cryptography;
+using System.Text;
+using System.Diagnostics;
+
+namespace RSALoginTest
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        private string BytesToHexString(byte[] input)
+        {
+            StringBuilder hexString = new StringBuilder(64);
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                hexString.Append(String.Format("{0:X2}", input[i]));
+            }
+            return hexString.ToString();
+        }
+
+        public static byte[] HexStringToBytes(string hex)
+        {
+            if (hex.Length == 0)
+            {
+                return new byte[] { 0 };
+            }
+
+            if (hex.Length % 2 == 1)
+            {
+                hex = "0" + hex;
+            }
+
+            byte[] result = new byte[hex.Length / 2];
+
+            for (int i = 0; i < hex.Length / 2; i++)
+            {
+                result[i] = byte.Parse(hex.Substring(2 * i, 2), System.Globalization.NumberStyles.AllowHexSpecifier);
+            }
+            
+            return result;
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            LoginResult = "";
+            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+            if (string.Compare(Request.RequestType, "get", true)==0)
+            {
+                //将私钥存Session中
+                Session["private_key"] = rsa.ToXmlString(true);
+            }
+            else
+            {
+                bool bLoginSucceed = false;
+                try
+                {
+                    string strUserName = Request.Form["txtUserName"];
+                    postbackUserName = strUserName;
+                    string strPwdToDecrypt = Request.Form["encrypted_pwd"];
+                    rsa.FromXmlString((string)Session["private_key"]);
+                    byte[] result = rsa.Decrypt(HexStringToBytes(strPwdToDecrypt), false);
+                    System.Text.ASCIIEncoding enc = new ASCIIEncoding();
+                    string strPwdMD5 = enc.GetString(result);
+                    if (string.Compare(strUserName, "user1", true)==0 && string.Compare(strPwdMD5, "14e1b600b1fd579f47433b88e8d85291", true)==0)
+                        bLoginSucceed = true;
+                }
+                catch (Exception)
+                {
+
+                }
+                if (bLoginSucceed)
+                    LoginResult = "登录成功";
+                else
+                    LoginResult = "登录失败";
+            }
+
+            //把公钥适当转换，准备发往客户端
+            RSAParameters parameter = rsa.ExportParameters(true);
+            strPublicKeyExponent = BytesToHexString(parameter.Exponent);
+            strPublicKeyModulus = BytesToHexString(parameter.Modulus);
+        }
+
+        protected string strPublicKeyExponent = "";
+        protected string strPublicKeyModulus = "";
+        protected string LoginResult = "";
+        protected string postbackUserName = "";
+    }
+}
